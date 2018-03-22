96c1b72d9f6662a0e92e5b3407737537b0284abb
IsaiahL12 <zaya1one@hotmail.com>
2018-03-22 12:53:09 -0400

    The first push

---
+++ src/Project/Display1.java
@@ -0,0 +1,29 @@
+package Project;
+
+import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+
+//import javax.imageio.ImageIO;
+import javax.swing.*;
+
+
+public class Display1 extends JFrame {
+	public static JFrame frame;
+	public static final int MINI_BORDER_X= 500;
+	public static final int MINI_BORDER_Y= 500;
+	
+	public static void main (String[] args) {
+				frame = new Display1();
+		frame.setMinimumSize(new Dimension(MINI_BORDER_X, MINI_BORDER_Y));
+		frame.pack(); // scale thing down so that every thing fits on the frame
+		frame.setTitle("STOCK TICKER :)");
+		frame.setLocationRelativeTo(null);  //Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);	
+			
+	 }
+}

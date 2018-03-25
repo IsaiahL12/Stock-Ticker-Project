66b2e8e4d7b62572cc56b0aededfbfa3cba02053
IsaiahL12 <zaya1one@hotmail.com>
2018-03-25 12:56:57 -0400

    Third commit On Search Panel

--- src/Project/Display1.java
+++ src/Project/Display1.java
@@ -13,14 +13,20 @@ import javax.swing.*;
 
 public class Display1 extends JFrame {
 	public static JFrame frame;
-	public static final int MINI_BORDER_X= 700;
-	public static final int MINI_BORDER_Y= 500;
+	
+	public static final int MINI_BORDER_X= 300;
+	public static final int MINI_BORDER_Y= 300;
+	
+	public static final int MAX_BORDER_X = 800;
+	public static final int MAX_BORDER_Y = 500;
 	
 	public static void main (String[] args) {
 		frame = new Display1();
 		frame.add(new SearchPage());
 		frame.setMinimumSize(new Dimension(MINI_BORDER_X, MINI_BORDER_Y));
-		frame.pack(); // scale thing down so that every thing fits on the frame
+		frame.setMaximumSize(new Dimension(MAX_BORDER_X, MAX_BORDER_Y));
+		//frame.pack(); // scale thing down so that every thing fits on the frame
+		frame.setSize(MAX_BORDER_X, MAX_BORDER_Y);
 		frame.setTitle("STOCK TICKER :)");
 		frame.setLocationRelativeTo(null);  //Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/Project/SearchPage.java
+++ src/Project/SearchPage.java
@@ -21,18 +21,44 @@ public class SearchPage extends JPanel {
 		JPanel leftHalf = new JPanel();
 		leftHalf.setLayout(new BorderLayout());
 		
+		// beginning of the code that makes the search bar
 		JTextField searchBox = new JTextField();
-		searchBox.setColumns(15);
+		searchBox.setColumns(16);
 		JLabel searchBoxLabel = new JLabel ("Stock Search");
 		JButton searchButton = new JButton("Search");
-		
-		
+
 		JPanel searchComponentsCombine = new JPanel();
 		searchComponentsCombine.setLayout(new FlowLayout());
 		searchComponentsCombine.add( searchBoxLabel);
 		searchComponentsCombine.add( searchBox);
 		searchComponentsCombine.add( searchButton);
 		leftHalf.add(searchComponentsCombine, BorderLayout.NORTH);
+		// end of the code that makes the search bar
+		
+		// beginning of the code that makes the result panel
+		JPanel resultPanel = new JPanel();
+		resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS));
+		
+		
+		for (int i = 0; i < 25; i++) {	
+			JButton stockButton= new JButton("Stock - " + i) {
+				{
+				 setSize(400, 200);
+		         setMaximumSize(getSize());
+				}
+			};
+			resultPanel.add(stockButton);
+		}
+		
+		
+		JScrollPane scrollPane = new JScrollPane(resultPanel);
+		scrollPane.setPreferredSize(new Dimension(100, 400));
+		
+		leftHalf.add(scrollPane, BorderLayout.CENTER);
+		// end of the code that makes the result panel
+		
+		JButton previousButton = new JButton("Previous");
+		JButton nextButton = new JButton("Next");
 		
 		// End of that makes the left 
 		

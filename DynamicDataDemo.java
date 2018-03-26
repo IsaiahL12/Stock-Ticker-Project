b3292ab7be4edf18180ec1eba3d4c09f9ebf4166
zhengqi Yang <35822645+newsocialistvillage@users.noreply.github.com>
2018-03-25 20:33:03 -0400

    Add files via upload

---
+++ DynamicDataDemo.java
@@ -0,0 +1,115 @@
+package softwareEng1;
+
+
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import demo.DynamicDataDemo1;
+
+@SuppressWarnings("serial")
+public class DynamicDataDemo extends ApplicationFrame implements ActionListener {
+
+    /** The time series data. */
+    private TimeSeries series;
+
+    /** The most recent value added. */
+    private double lastValue = 100.0;
+
+    /**
+     * Constructs a new demonstration application.
+     *
+     * @param title  the frame title.
+     */
+    @SuppressWarnings("deprecation")
+	public DynamicDataDemo(final String title) {
+
+        super(title);
+        this.series = new TimeSeries("Random Data", Millisecond.class);
+        final TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
+        final JFreeChart chart = createChart(dataset);
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        final JButton button = new JButton("Add New Data Item");
+        button.setActionCommand("ADD_DATA");
+        button.addActionListener(this);
+
+        final JPanel content = new JPanel(new BorderLayout());
+        content.add(chartPanel);
+        content.add(button, BorderLayout.SOUTH);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(content);
+
+    }
+
+    /**
+     * Creates a sample chart.
+     * 
+     * @param dataset  the dataset.
+     * 
+     * @return A sample chart.
+     */
+    private JFreeChart createChart(final XYDataset dataset) {
+        final JFreeChart result = ChartFactory.createTimeSeriesChart(
+            "Stock Ticker", 
+            "Time Interval", 
+            "Price",
+            dataset, 
+            true, 
+            true, 
+            false
+        );
+        final XYPlot plot = result.getXYPlot();
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setAutoRange(true);
+        axis.setFixedAutoRange(60000.0);  // 60 seconds
+        axis = plot.getRangeAxis();
+        axis.setRange(0.0, 200.0); 
+        return result;
+    }
+    
+    
+    public void actionPerformed(final ActionEvent e) {
+        if (e.getActionCommand().equals("ADD_DATA")) {
+            final double factor = 0.90 + 0.2 * Math.random();
+            this.lastValue = this.lastValue * factor;
+            final Millisecond now = new Millisecond();
+            System.out.println("Now = " + now.toString());
+            this.series.add(new Millisecond(), this.lastValue);
+        }
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(final String[] args) {
+
+        final DynamicDataDemo demo = new DynamicDataDemo("Dynamic Data Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+
+}
+
+
+

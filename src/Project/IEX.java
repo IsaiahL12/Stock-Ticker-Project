8690b58db99050e7e8b5a32540a162fff4bebdb7
IsaiahL12 <zaya1one@hotmail.com>
2018-04-04 14:15:44 -0400

    Connected to all API and add the IEX trading API(4)(Sprint 2)

--- src/Project/IEX.java
+++ src/Project/IEX.java
@@ -13,53 +13,62 @@ public class IEX {
 	//IEX does not need a API key 
 	public static final String IEXWebisite = "https://ws-api.iextrading.com/1.0/";
 	public static final String refData = "https://api.iextrading.com/1.0/ref-data/symbols";
-	public  static Vector<String>  hold = new Vector<String>();
+	public  static Vector<String>  find = new Vector<String>();
 	public  static Vector<String>  parse = new Vector<String>();
 	public  static Vector<String>  savedResult = new Vector<String>();
-	public IEX() throws IOException, JSONException  {
+	
+	
+	public IEX(){
 		
 	}
 	
 	
 
-	 private static  Vector<String>  readAll(Reader rd) throws IOException {
+	 private static  Vector<String>  readAllForVector(Reader rd) throws IOException {
 		    StringBuilder sb = new StringBuilder();
 		    int cp;
 		    while ((cp = rd.read()) != -1) {
 		      sb.append((char) cp);
 		      if ('}'== cp) {
-		    	 hold.add(sb.toString().toLowerCase());
+		    	  find.add(sb.toString());
 		    	 sb = new StringBuilder();
+		      }
 		    }
-		    }
-		    return hold;
+		    return find;
 		  }
 	  
 
-	  public static Vector<String> readJsonFromUrl(String url) throws IOException {
+	  public static Vector<String> readUrlToVector(String url) throws IOException {
 	    InputStream is = new URL(url).openStream();
 	    try {
 	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      Vector<String> jsonText = readAll(rd);
-	      return jsonText;
+	      Vector<String> textVector = readAllForVector(rd);
+	      return textVector;
 	    } finally {
 	      is.close();
 	    }
 	  }
 	
-	public static void main(String [] args) throws IOException, JSONException {
-		 Vector<String> getData = readJsonFromUrl(refData);
-		 String a = "\"name\":\"A";
-		 //JSONObject json = new JSONObject(getData);
+	public Vector<String> searchForByNames(String jtextfeild) throws IOException {
+		Vector<String> getData = readUrlToVector(refData);
+		 String search = "\"name\":\"";
+		 search = search + jtextfeild;
 		 for (int i = 0; i < getData.size(); i++) {
-			 if(getData.get(i).contains(a.toLowerCase())) {
-		 System.out.println(getData.get(i));
+			 if(getData.get(i).toLowerCase().contains(search.toLowerCase())) {
+				 parse.add(getData.get(i));
 			 }
 		 }
-			}
-	
-	public String searchForByNames() {
-		return null;
+		 for (int i = 0; i < parse.size(); i++) {
+			 String[] parsingString =parse.get(i).split(":");
+			 parsingString = parsingString[2].split("\"");
+			 savedResult.add(parsingString[1]);
+		 }
+		 
+		return savedResult;
 	}
 	
+	public static void main(String [] args) throws IOException, JSONException {
+		IEX a= new IEX();
+		System.out.println(a.searchForByNames("AG"));
+			}
 }
